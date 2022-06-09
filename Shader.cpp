759ebdeaba283b1e52a9192ead90c24bf2311430
polyqubit <polyumbrial@gmail.com>
2022-06-08 22:09:53 -0700

    completed shader class

--- Shader.cpp
+++ Shader.cpp
@@ -37,10 +37,12 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 	unsigned int vertex, fragment;
 	int success;
 	char infoLog[512];
+
 	// vertex Shader
 	vertex = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertex, 1, &vShaderCode, NULL);
 	glCompileShader(vertex);
+
 	// print compile errors if any
 	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
 	if (!success)
@@ -49,11 +51,25 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
 			infoLog << std::endl;
 	};
+
 	// similiar for Fragment Shader
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &vShaderCode, NULL);
+	glCompileShader(fragment);
+	// print compile errors if any
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" <<
+			infoLog << std::endl;
+	};
+
 	ID = glCreateProgram();
 	glAttachShader(ID, vertex);
 	glAttachShader(ID, fragment);
 	glLinkProgram(ID);
+
 	// print linking errors if any
 	glGetProgramiv(ID, GL_LINK_STATUS, &success);
 	if (!success)
@@ -62,10 +78,10 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" <<
 			infoLog << std::endl;
 	}
+
 	// delete shaders; they’re linked into our program and no longer necessary
 	glDeleteShader(vertex);
 	glDeleteShader(fragment);
-
 }
 
 void Shader::use()
@@ -73,3 +89,15 @@ void Shader::use()
 	glUseProgram(ID);
 }
 
+void Shader::setBool(const std::string& name, bool value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+void Shader::setInt(const std::string& name, int value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+void Shader::setFloat(const std::string& name, float value) const
+{
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
